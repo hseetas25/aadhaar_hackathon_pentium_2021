9b7b3ea1b5761b909789cd13f9589e7edfba0afb
Roshan Kumar <58880369+roshankumar2303@users.noreply.github.com>
2021-10-28 22:16:33 +0530

    Revert "Added components and Routing"
    This reverts commit 0fd1c0624f14c4ff0c1f5dcc7fd0c2bc57a1a8dd.

--- Aadhaar-Address-Update/src/app/app-routing.module.ts
+++ Aadhaar-Address-Update/src/app/app-routing.module.ts
@@ -1,15 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BorrowerComponent } from './borrower/borrower.component';
-import { HomeComponent } from './home/home.component';
-import { LandlordComponent } from './landlord/landlord.component';
 
-const routes: Routes = [
-    {path:"", redirectTo:"/home", pathMatch:"full"},
-    {path:"home", component:HomeComponent},
-    {path:"landlord", component:LandlordComponent},
-    {path:"borrower", component:BorrowerComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Aadhaar-Address-Update/src/app/app.module.ts
+++ Aadhaar-Address-Update/src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { LandlordComponent } from './landlord/landlord.component';
-import { BorrowerComponent } from './borrower/borrower.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    LandlordComponent,
-    BorrowerComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
